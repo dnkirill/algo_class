1c4119fd8f3663ac1d3a3d915cb8c809e0ce6188
Kirill Danilyuk <dnkirill@gmail.com>
2012-06-13 20:37:12 +0400

    (ruby) MergeSort: made it readable

--- lectures_code/ruby/1_3_merge_sort.rb
+++ lectures_code/ruby/1_3_merge_sort.rb
@@ -1,42 +1,20 @@
-module Sort
-  def self.merge_sort(arr)
-    return arr.to_a if arr.size < 2
-    left_part, right_part = [], []
-    arr.each_with_index do |elem, i|
-      if i < arr.size / 2
-        left_part << elem
-      else
-        right_part << elem
-      end
+class Sort
+  class << self
+    def merge_sort(arr)
+      return arr if arr.size < 2
+      half  = arr.size / 2
+      left  = arr.first half
+      right = arr.drop half
+      merge(merge_sort(left), merge_sort(right))
     end
-    left_array  = merge_sort(left_part)
-    right_array = merge_sort(right_part)
-    return merge(left_array, right_array)
-  end
 
-  def self.merge(left_array, right_array)
-    p "la: #{left_array}; ra: #{right_array}"
-    sorted_array = Array.new
-    li, ri, la, ra = 0, 0, left_array, right_array
-    while (li < la.size) && (ri < ra.size)
-      left_value, right_value = la[li], ra[ri]
-      if left_value < right_value
-        sorted_array << left_value
-        li += 1
-      else
-        sorted_array << right_value
-        ri += 1
+    def merge(left, right)
+      sorted_array = Array.new
+      until left.empty? or right.empty?
+        elem = left.first < right.first ? left.shift : right.shift
+        sorted_array << elem
       end
+      sorted_array + left + right
     end
-    while li < la.size
-      sorted_array << la[li]
-      li += 1
-    end
-    while ri < ra.size
-      sorted_array << ra[ri]
-      ri += 1
-    end
-    p "resulted: #{sorted_array}"
-    return sorted_array
   end
 end
