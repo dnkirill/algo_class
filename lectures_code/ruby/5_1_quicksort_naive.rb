15ff93c8d78eea7283ed5783dde694d0a98e33c4
Kirill Danilyuk <dnkirill@gmail.com>
2012-07-05 11:51:14 +0400

    (~) Added partitioning algo from lecture

--- lectures_code/ruby/5_1_quicksort_naive.rb
+++ lectures_code/ruby/5_1_quicksort_naive.rb
@@ -4,7 +4,7 @@ class QuickSort
   def initialize(array)
     @arr = array
 		p "Entry array:"
-		p array
+		p @arr
 	end
 
   def sort
@@ -14,8 +14,8 @@ class QuickSort
   end
 
   def quicksort(start, finish)
-    return if start - finish == 1
-    pivot = partition(start, finish)
+    return if finish - start <= 1
+    pivot = partition2(start, finish)
     quicksort(start, pivot - 1)
     quicksort(pivot + 1, finish)
   end
@@ -33,5 +33,26 @@ class QuickSort
       end
     end
     return pivot
+  end
+
+  # Partitioning algo from lecture
+  def partition2(start, finish)
+    pivot = start
+    lh = rh = pivot + 1
+    (rh..finish).each do |rh|
+      if @arr[rh] < @arr[pivot]
+        swap!(lh, rh)
+        lh += 1
+      end
+    end
+    if @arr[lh - 1] < @arr[pivot]
+      swap!(lh - 1, pivot) 
+      pivot = lh - 1
+    end
+    pivot
+  end
+
+  def swap!(el1, el2)
+    @arr[el1], @arr[el2] = @arr[el2], @arr[el1]
   end
 end
